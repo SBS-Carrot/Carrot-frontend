bf46dd0cffe8da008a1bb38728f5a7588468961e
xanqus <sbk98031816@gmail.com>
2022-07-29 15:16:59 +0900

    시간 변환

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import React, { useState } from "react";
 function App() {
-  return <div></div>;
+  const [minutes, setMinutes] = useState(0);
+  return (
+    <div>
+      <div>Time Converter</div>
+      <div>
+        Minutes{" "}
+        <input
+          placeholder="Minutes"
+          value={minutes}
+          type="number"
+          onChange={(event) => {
+            setMinutes(event.target.value);
+          }}
+        />
+        <div>
+          Hours
+          <input
+            type="number"
+            placeholder="Hours"
+            value={Math.floor(minutes / 60)}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;

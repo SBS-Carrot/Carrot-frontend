a493029283e1cc56bdac22dc6fc9d81a0f1e5c4f
xanqus <sbk98031816@gmail.com>
2022-07-29 15:35:54 +0900

    active toggle 버튼

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
 function App() {
   const [minutes, setMinutes] = useState(0);
+  const [active, setActive] = useState(false);
+  const onChange = (event) => {
+    setMinutes(event.target.value);
+  };
+  const reset = () => {
+    setMinutes(0);
+  };
+  const changeActive = () => {
+    setActive(!active);
+  };
+
   return (
     <div>
       <div>Time Converter</div>
@@ -10,18 +21,20 @@ function App() {
           placeholder="Minutes"
           value={minutes}
           type="number"
-          onChange={(event) => {
-            setMinutes(event.target.value);
-          }}
+          onChange={onChange}
+          disabled={active}
         />
         <div>
-          Hours
+          Hours{" "}
           <input
             type="number"
             placeholder="Hours"
             value={Math.floor(minutes / 60)}
+            disabled={!active}
           />
         </div>
+        <button onClick={reset}>reset</button>
+        <button onClick={changeActive}>active toggle</button>
       </div>
     </div>
   );

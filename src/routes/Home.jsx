84fa43df43581e479d205c03e182fb147e1b0067
byunghoonyoon <qudgns0218@gmail.com>
2022-10-11 21:47:01 +0900

    master브랜치

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -10,8 +10,12 @@ const Home = () => {
       <Header />
       <div>Home 입니다</div>
       <div className="border-8">테일윈드 적용</div>
+      <span> master branch 추가했습니다</span>
     </div>
   );
 };
 
 export default Home;
+
+//https://goddaehee.tistory.com/274 Branch 기초생성 및 이동
+//https://goddaehee.tistory.com/275?category=381481 Branch 병합

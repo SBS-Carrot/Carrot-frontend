d2092978492e34ee0a334502c8a9e93056082427
hyelim3 <shl0703ss@gmail.com>
2022-10-12 17:46:14 +0900

    첫페이지 이미지

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,19 +2,58 @@ import React from "react";
 import Header from "../layouts/Header";
 import { useRecoilValue } from "recoil";
 import { authenticatedState } from "../recoil/auth";
+import "../styles/Home.css";
 
 const Home = () => {
   const logined = useRecoilValue(authenticatedState);
   return (
     <div>
       <Header />
-      <div>Home 입니다</div>
-      <div className="border-8">테일윈드 적용</div>
-      <span> master branch 추가했습니다</span>
-      <span> test branch bug fix했습니다</span>
-      <span> master branch 에서 작업 추가했습니다.</span>
-      <span> test branch 에서 작업 추가했습니다.</span>
-      <span> 혜림 branch 추가했습니다 </span>
+      <div className="background1 flex items-center">
+        <div
+          className="flex"
+          style={{
+            width: "1000px",
+
+            margin: "0 auto",
+          }}
+        >
+          <div className="flex justify-center flex-col">
+            <div
+              className="font-bold"
+              style={{
+                fontSize: "3rem",
+              }}
+            >
+              <div
+                style={{
+                  width: "300px",
+                }}
+              >
+                당신 근처의
+              </div>
+              <div>당근마켓</div>
+            </div>
+            <div
+              className="mt-8"
+              style={{
+                width: "330px",
+              }}
+            >
+              <div>중고 거래부터 동네 정보까지, 이웃과 함께해</div>
+              <div>가깝고 따듯한 당신의 근처를 만들어요.</div>
+            </div>
+          </div>
+          <div>
+            <div className="firstimg">
+              <img
+                src="https://d1unjqcospf8gs.cloudfront.net/assets/home/main/3x/image-top-d6869a79bc4cb58ea59aa5a408decfdf4a4ba60ac639837081da12861083cdbb.webp"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

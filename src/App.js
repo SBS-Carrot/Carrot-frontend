9acbd911c9a1eebc602be608d80bc5f3331e5767
byunghoonyoon <qudgns0218@gmail.com>
2022-10-12 17:30:33 +0900

    trust 사이트 front 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Home from "./routes/Home";
+import Trust from "./routes/Trust";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/trust" element={<Trust />} />
       </Routes>
     </Router>
   );

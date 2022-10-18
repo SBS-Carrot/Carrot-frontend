e1788fe1e45019db58812a42804893c115fa344c
byunghoonyoon <qudgns0218@gmail.com>
2022-10-18 18:46:03 +0900

    회원가입창 수정

--- src/routes/Join.jsx
+++ src/routes/Join.jsx
@@ -4,8 +4,14 @@ import Header from "../layouts/Header";
 const Join = () => {
   const [idValue, setIdValue] = useState("");
   const [passwordValue, setPasswordValue] = useState("");
+  const [password1Value, setPassword1Value] = useState("");
   const [nameValue, setNameValue] = useState("");
-  const [birthValue, setBirthValue] = useState("");
+  const [birthValue, setBirthValue] = useState({
+    yy: "",
+    mm: "",
+    dd: "",
+  });
+
   const [addressValue, setAddressValue] = useState("");
   const [emailValue, setEmailValue] = useState("");
   const [nicknameValue, setNicknameValue] = useState("");
@@ -17,11 +23,26 @@ const Join = () => {
   const onPasswordChange = (e) => {
     setPasswordValue(e.target.value);
   };
+  const onPassword1Change = (e) => {
+    setPassword1Value(e.target.value);
+  };
   const onNameChange = (e) => {
     setNameValue(e.target.value);
   };
-  const onBirthChange = (e) => {
-    setBirthValue(e.target.value);
+  const onYearChange = (e) => {
+    setBirthValue((prev) => {
+      return { ...prev, yy: e.target.value };
+    });
+  };
+  const onMonthChange = (e) => {
+    setBirthValue((prev) => {
+      return { ...prev, mm: e.target.value };
+    });
+  };
+  const onDayChange = (e) => {
+    setBirthValue((prev) => {
+      return { ...prev, dd: e.target.value };
+    });
   };
   const onAddressChange = (e) => {
     setAddressValue(e.target.value);
@@ -34,6 +55,38 @@ const Join = () => {
   };
   const onPhoneChange = (e) => {
     setPhoneValue(e.target.value);
+  };
+
+  const onJoin = async (
+    idValue,
+    passwordValue,
+    password1Value,
+    nameValue,
+    birthValue,
+    addressValue,
+    emailValue,
+    nicknameValue,
+    phoneValue
+  ) => {
+    try {
+      const data = await axios.get(`http://localhost:8082/create`);
+    } catch (e) {
+      // try {
+      //   const data = await axios.post(`http://localhost:8082/create`, {
+      //     userid: idValue,
+      //     password: passwordValue,
+      //     password2: password1Value,
+      //     username: nameValue,
+      //     birth: birthValue,
+      //     email: emailValue,
+      //     nickname: nicknameValue,
+      //     address: addressValue,
+      //     phone: phoneValue,
+      //   });
+      //   window.alert("회원가입 완료");
+      // }
+      console.log(e);
+    }
   };
   return (
     <div>
@@ -77,6 +130,8 @@ const Join = () => {
           <input
             type="text"
             placeholder="아이디를 입력해 주세요"
+            value={idValue}
+            onChange={onIdChange}
             style={{
               outline: "1px gray solid",
               width: "400px",
@@ -97,6 +152,8 @@ const Join = () => {
           <input
             type="password"
             placeholder="비밀번호는 8자 이상 20자 이하, 대문자+소문자+숫자"
+            value={passwordValue}
+            onChange={onPasswordChange}
             style={{
               outline: "1px gray solid",
               width: "400px",
@@ -117,6 +174,8 @@ const Join = () => {
           <input
             type="password"
             placeholder="비밀번호는 8자 이상 20자 이하, 대문자+소문자+숫자"
+            value={password1Value}
+            onChange={onPassword1Change}
             style={{
               outline: "1px gray solid",
               width: "400px",
@@ -137,6 +196,8 @@ const Join = () => {
           <input
             type="text"
             placeholder="이름을 입력해 주세요"
+            value={nameValue}
+            onChange={onNameChange}
             style={{
               outline: "1px gray solid",
               width: "400px",
@@ -162,117 +223,27 @@ const Join = () => {
               margin: "0 auto",
             }}
           >
-            <select
-              placeholder="생년"
+            <input
+              type="text"
+              placeholder="생년(4자)"
+              onChange={onYearChange}
+              value={birthValue.yy}
               style={{
                 outline: "1px gray solid",
                 width: "125px",
                 height: "50px",
               }}
-            >
-              <option selected value="연도" disabled>
-                연도
-              </option>
-              <option value="1930">1930</option>
-              <option value="1931">1931</option>
-              <option value="1932">1932</option>
-              <option value="1933">1933</option>
-              <option value="1934">1934</option>
-              <option value="1935">1935</option>
-              <option value="1936">1936</option>
-              <option value="1937">1937</option>
-              <option value="1938">1938</option>
-              <option value="1939">1939</option>
-              <option value="1940">1940</option>
-              <option value="1941">1941</option>
-              <option value="1942">1942</option>
-              <option value="1943">1943</option>
-              <option value="1944">1944</option>
-              <option value="1945">1945</option>
-              <option value="1946">1946</option>
-              <option value="1947">1947</option>
-              <option value="1948">1948</option>
-              <option value="1949">1949</option>
-              <option value="1950">1950</option>
-              <option value="1951">1951</option>
-              <option value="1952">1952</option>
-              <option value="1953">1953</option>
-              <option value="1954">1954</option>
-              <option value="1955">1955</option>
-              <option value="1956">1956</option>
-              <option value="1957">1957</option>
-              <option value="1958">1958</option>
-              <option value="1959">1959</option>
-              <option value="1960">1960</option>
-              <option value="1961">1961</option>
-              <option value="1962">1962</option>
-              <option value="1963">1963</option>
-              <option value="1964">1964</option>
-              <option value="1965">1965</option>
-              <option value="1966">1966</option>
-              <option value="1967">1967</option>
-              <option value="1968">1968</option>
-              <option value="1969">1969</option>
-              <option value="1970">1970</option>
-              <option value="1971">1971</option>
-              <option value="1972">1972</option>
-              <option value="1973">1973</option>
-              <option value="1974">1974</option>
-              <option value="1975">1975</option>
-              <option value="1976">1976</option>
-              <option value="1977">1977</option>
-              <option value="1978">1978</option>
-              <option value="1979">1979</option>
-              <option value="1980">1980</option>
-              <option value="1981">1981</option>
-              <option value="1982">1982</option>
-              <option value="1983">1983</option>
-              <option value="1984">1984</option>
-              <option value="1985">1985</option>
-              <option value="1986">1986</option>
-              <option value="1987">1987</option>
-              <option value="1988">1988</option>
-              <option value="1989">1989</option>
-              <option value="1990">1990</option>
-              <option value="1991">1991</option>
-              <option value="1992">1992</option>
-              <option value="1993">1993</option>
-              <option value="1994">1994</option>
-              <option value="1995">1995</option>
-              <option value="1996">1996</option>
-              <option value="1997">1997</option>
-              <option value="1998">1998</option>
-              <option value="1999">1999</option>
-              <option value="2000">2000</option>
-              <option value="2001">2001</option>
-              <option value="2002">2002</option>
-              <option value="2003">2003</option>
-              <option value="2004">2004</option>
-              <option value="2005">2005</option>
-              <option value="2006">2006</option>
-              <option value="2007">2007</option>
-              <option value="2008">2008</option>
-              <option value="2009">2009</option>
-              <option value="2010">2010</option>
-              <option value="2011">2011</option>
-              <option value="2012">2012</option>
-              <option value="2013">2013</option>
-              <option value="2014">2014</option>
-              <option value="2015">2015</option>
-              <option value="2016">2016</option>
-              <option value="2017">2017</option>
-            </select>
+            />
             <select
               placeholder="월"
+              value={birthValue.mm}
+              onChange={onMonthChange}
               style={{
                 outline: "1px gray solid",
                 width: "125px",
                 height: "50px",
               }}
             >
-              <option selected value="월" disabled>
-                월
-              </option>
               <option value="01">1월</option>
               <option value="02">2월</option>
               <option value="03">3월</option>
@@ -286,26 +257,26 @@ const Join = () => {
               <option value="11">11월</option>
               <option value="12">12월</option>
             </select>
+
             <select
               placeholder="일"
+              onChange={onDayChange}
+              value={birthValue.dd}
               style={{
                 outline: "1px gray solid",
                 width: "125px",
                 height: "50px",
               }}
             >
-              <option selected value="일" disabled>
-                일
-              </option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
+              <option value="01">1</option>
+              <option value="02">2</option>
+              <option value="03">3</option>
+              <option value="04">4</option>
+              <option value="05">5</option>
+              <option value="06">6</option>
+              <option value="07">7</option>
+              <option value="08">8</option>
+              <option value="09">9</option>
               <option value="10">10</option>
               <option value="11">11</option>
               <option value="12">12</option>
@@ -344,6 +315,8 @@ const Join = () => {
           <input
             type="text"
             placeholder="대전시 OO구 OO동까지 입력해주세요"
+            value={addressValue}
+            onChange={onAddressChange}
             style={{
               outline: "1px gray solid",
               width: "400px",
@@ -364,6 +337,8 @@ const Join = () => {
           <input
             type="text"
             placeholder="이메일을 입력해 주세요"
+            value={emailValue}
+            onChange={onEmailChange}
             style={{
               outline: "1px gray solid",
               width: "400px",
@@ -384,6 +359,8 @@ const Join = () => {
           <input
             type="text"
             placeholder="당근마켓 내에서 사용할 닉네임 입력"
+            value={nicknameValue}
+            onChange={onNicknameChange}
             style={{
               outline: "1px gray solid",
               width: "400px",
@@ -404,6 +381,8 @@ const Join = () => {
           <input
             type="text"
             placeholder="전화번호 입력"
+            onChange={onPhoneChange}
+            value={phoneValue}
             style={{
               outline: "1px gray solid",
               width: "400px",
@@ -420,6 +399,19 @@ const Join = () => {
                 backgroundColor: "#ffa445",
                 padding: "15px 20px",
                 borderRadius: "10px",
+              }}
+              onClick={() => {
+                onJoin(
+                  idValue,
+                  passwordValue,
+                  password1Value,
+                  nameValue,
+                  birthValue,
+                  addressValue,
+                  emailValue,
+                  nicknameValue,
+                  phoneValue
+                );
               }}
             >
               가입하기

bbf5fbfd3ebb45f56792e3287c9294b0a0cc5d13
hyelim3 <shl0703ss@gmail.com>
2022-11-01 17:54:35 +0900

    알바공고

--- src/components/JobsWrite.jsx
+++ src/components/JobsWrite.jsx
@@ -14,6 +14,7 @@ const JobsWrite = () => {
   const [timeValue, setTimeValue] = useState("");
   const [completeToggle, setCompleteToggle] = useState(false);
   const [showImages, setShowImages] = useState([]);
+  const [day, setDay] = useState([]);
   const [id, setId] = useState("");
   const onPriceChange = (e) => {
     setPriceValue(e.target.value);
@@ -88,6 +89,22 @@ const JobsWrite = () => {
       console.log(e);
     }
   };
+
+  const DayArray = (e) => {
+    console.log(e.target.value);
+    setDay([...e.target.value, ...day]);
+    console.log(day);
+  };
+
+  const DayDelete = (id) => {
+    //요일 삭제
+    for (let i = 0; i < day.length; i++) {
+      if (id == i) {
+        day.splic(id, 1);
+      }
+    }
+  };
+
   const onSubmits = async (
     subjectValue,
     contentValue,
@@ -210,6 +227,9 @@ const JobsWrite = () => {
             <div className="pt-2">일하는 요일</div>
             <div className="flex my-2 gap-2">
               <button
+                onClick={() => {
+                  DayArray();
+                }}
                 className="flex  rounded-full"
                 style={{
                   width: "40px",
@@ -230,6 +250,8 @@ const JobsWrite = () => {
               </button>
               <button
                 className="flex  rounded-full"
+                onChange={DayArray}
+                // onClick={() => DayArray()}
                 style={{
                   width: "40px",
                   height: "40px",
@@ -249,6 +271,8 @@ const JobsWrite = () => {
               </button>{" "}
               <button
                 className="flex  rounded-full"
+                onChange={DayArray}
+                // onClick={() => DayArray()}
                 style={{
                   width: "40px",
                   height: "40px",
@@ -355,8 +379,30 @@ const JobsWrite = () => {
                 width: "100px",
               }}
             >
+              <option value="24:00">00:00</option>
+              <option value="01:00">01:00</option>
+              <option value="02:00">02:00</option>
+              <option value="03:00">03:00</option>
+              <option value="04:00">04:00</option>
+              <option value="05:00">05:00</option>
+              <option value="06:00">06:00</option>
+              <option value="07:00">07:00</option>
+              <option value="08:00">08:00</option>
               <option value="09:00">09:00</option>
+              <option value="10:00">10:00</option>
+              <option value="11:00">11:00</option>
+              <option value="12:00">12:00</option>
+              <option value="13:00">13:00</option>
+              <option value="14:00">14:00</option>
+              <option value="15:00">15:00</option>
+              <option value="16:00">16:00</option>
+              <option value="17:00">17:00</option>
               <option value="18:00">18:00</option>
+              <option value="19:00">19:00</option>
+              <option value="20:00">20:00</option>
+              <option value="21:00">21:00</option>
+              <option value="22:00">22:00</option>
+              <option value="23:00">23:00</option>
             </select>
             <span> ~ </span>
             <select
@@ -365,7 +411,30 @@ const JobsWrite = () => {
                 border: "1px #d5d5d5 solid",
               }}
             >
+              <option value="24:00">00:00</option>
+              <option value="01:00">01:00</option>
+              <option value="02:00">02:00</option>
+              <option value="03:00">03:00</option>
+              <option value="04:00">04:00</option>
+              <option value="05:00">05:00</option>
+              <option value="06:00">06:00</option>
+              <option value="07:00">07:00</option>
+              <option value="08:00">08:00</option>
+              <option value="09:00">09:00</option>
+              <option value="10:00">10:00</option>
+              <option value="11:00">11:00</option>
+              <option value="12:00">12:00</option>
+              <option value="13:00">13:00</option>
+              <option value="14:00">14:00</option>
+              <option value="15:00">15:00</option>
+              <option value="16:00">16:00</option>
+              <option value="17:00">17:00</option>
               <option value="18:00">18:00</option>
+              <option value="19:00">19:00</option>
+              <option value="20:00">20:00</option>
+              <option value="21:00">21:00</option>
+              <option value="22:00">22:00</option>
+              <option value="23:00">23:00</option>
             </select>
           </div>
           <div className="pt-2">
